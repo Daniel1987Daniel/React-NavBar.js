a931b1f70f87889c2be44c2bd927dab7067cd8a0
Daniel Sládeček <83771369+Daniel1987Daniel@users.noreply.github.com>
2022-11-28 14:31:48 +0100

    Add files via upload

---
+++ Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <nav>
+      <img src="https://th.bing.com/th/id/R.f81a6f373c244b1f70f4b7402b5ab372?rik=rbXh4ieLuKt%2bmA&riu=http%3a%2f%2flogos-download.com%2fwp-content%2fuploads%2f2016%2f09%2fReact_logo_logotype_emblem.png&ehk=QhGOkKcUKCU7FBQgHOajOiJqJBACUTD2Ni6LsfqzCEA%3d&risl=&pid=ImgRaw&r=0" alt="logo" height={60}/>
+      <h3>ReactFacts </h3>
+      <h4>React course - Project 1</h4>
+    </nav>
+  )
+};
